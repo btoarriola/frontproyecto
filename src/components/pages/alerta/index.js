e1b5ab35d65c8e63ec87c45dd5aad8c2e82b65dd
btoarriola <betokanon@gmail.com>
2023-05-09 18:37:22 -0600

    add usuario

--- src/components/pages/alerta/index.js
+++ src/components/pages/alerta/index.js
@@ -1,47 +1,56 @@
 import React from 'react';
-import { useState} from 'react';
+import { useState } from 'react';
 import { MdWbTwighlight } from 'react-icons/md';
 
 import './alerta.css';
 import Header from '../../common/Header'
 
 function Alerta() {
-    const [numLlamada, setNumLlamada] = useState('');
-    const [numMensaje, setNumMensaje] = useState('');
-    const [mensaje, setMensaje] = useState('');
+  const [formData, setFormData] = useState({
+    numLlamada: '',
+    numMensaje: '',
+    mensaje: '',
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(numLlamada, numMensaje, mensaje);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
   
   return (
-      <div className="Alerta">
-        <Header/>
-        <span id="title">Alerta</span>
-        <div id="alertacontainer">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <p className="nota">Se llamará al número si activa el botón de alerta</p>
-                    <input type="number" value={numLlamada} onChange={(e) => setNumLlamada(e.target.value)} placeholder=' Introduzca el numero de telefono'/>
-                    
-                </div>
-                <div>
-                    <p className="nota">Se enviará el mensaje si activa el botón de alerta</p>
-                    <input type="number" value={numMensaje} onChange={(e) => setNumMensaje(e.target.value)} placeholder=' Introduzca el numero de telefono'/>
-                </div>
-                <div>
-                <p className="nota">Escriba el mensaje a enviar.</p>
-                    <textarea value={mensaje} onChange={(e) => setMensaje(e.target.value)} placeholder='Introduzca el mensaje'/>
-                </div>
-                <button type="submit" className='botonaplicar'>Aplicar</button>
-            </form>
+    <div className="Alerta">
+      <Header/>
+      <span id="title">Alerta</span>
+      <div id="alertacontainer">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p className="nota">Se llamará al número si activa el botón de alerta</p>
+            <input type="number" name="numLlamada" value={formData.numLlamada} onChange={handleChange} placeholder=' Introduzca el numero de telefono'/>
+          </div>
+          <div>
+            <p className="nota">Se enviará el mensaje si activa el botón de alerta</p>
+            <input type="number" name="numMensaje" value={formData.numMensaje} onChange={handleChange} placeholder=' Introduzca el numero de telefono'/>
+          </div>
+          <div>
+            <p className="nota">Escriba el mensaje a enviar.</p>
+            <textarea name="mensaje" value={formData.mensaje} onChange={handleChange} placeholder='Introduzca el mensaje'/>
+          </div>
+          <button type="submit" className='botonaplicar'>Aplicar</button>
+        </form>
 
-            <div className='botoncontainer'>
-                <MdWbTwighlight size={110} id="iconboton"/>
-            </div>
+        <div className='botoncontainer'>
+          <MdWbTwighlight size={110} id="iconboton"/>
         </div>
       </div>
+    </div>
   );
 }
 
